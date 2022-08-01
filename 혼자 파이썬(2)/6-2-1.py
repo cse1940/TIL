35a82f9365b7f8f5a4778bc81bad8780b3a7e071
cse1940 <se1940@naver.com>
2022-08-02 00:26:24 +0900

    코딩생활

---
+++ 혼자 파이썬(2)/6-2-1.py
@@ -0,0 +1,13 @@
+# 두 숫자를 비교한 결과를 출력하는 코드입니다.
+
+a = 10
+b = 7
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+# 비교 연산자를 이용해 a와 b를 비교한 결과를 출력했습니다.
+# 비교문의 결과는 모두 불 자료형인 True 또는 False로 나타남.
