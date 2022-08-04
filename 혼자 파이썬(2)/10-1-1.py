6b392db9750948e129e171f2cb3fdf2bfa696df0
cse1940 <se1940@naver.com>
2022-08-04 10:05:57 +0900

    코딩생활

---
+++ 혼자 파이썬(2)/10-1-1.py
@@ -0,0 +1,6 @@
+# value라는 리스트에 값들을 저장하고, 저장된 값을 가져와 출력하는 코드입니다.
+
+value = [10, 9, 8, 9, 9]
+print(value[0])
+print(value[3])
+print(value[-1])
---
+++ 혼자 파이썬(2)/10-4-6.py
@@ -0,0 +1,7 @@
+# reverse() 함수로 순서를 뒤바꾸는 코드입니다.
+
+a = [-1, 4, 2, 6, 3]
+a.reverse()
+print(a)
+
+print(a[::-1])
