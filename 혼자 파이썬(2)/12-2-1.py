9b244a76ef86e6f9a8cf8d7ac675adaa163b2b12
cse1940 <se1940@naver.com>
2022-08-05 12:11:31 +0900

    코딩생활

---
+++ 혼자 파이썬(2)/12-2-1.py
@@ -0,0 +1,7 @@
+# 합칩합
+
+a = set([1, 2, 3])
+b = set([2, 3, 4])
+print(a | b)
+print(a.union(b))
+print(b.union(a))
---
+++ 혼자 파이썬(2)/12-2-3.py
@@ -0,0 +1,8 @@
+# 차집합
+
+a = set([1, 2, 3])
+b = set([2, 3, 4])
+print(a - b)
+print(b - a)
+print(a.difference(b))
+print(b.difference(a))
