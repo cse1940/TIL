d5eee4b024275bf5820cb0e395d95e7081c7089d
cse1940 <se1940@naver.com>
2022-08-14 00:48:15 +0900

    with_java

--- practice_java/ch01_HelloWorld/HelloWorld.java
+++ practice_java/ch01_HelloWorld/HelloWorld.java
@@ -1,7 +1,5 @@
-package com.yudong80.java; //패키지를 선언한 경우 해당 폴더 위치에 있을 것
- 
 public class HelloWorld { //자바 파일의 이름은 public class의 이름과 동일할 것
     public static void main(String[] args) { //main() 함수는 단독으로 존재할 수 없으며 public class에 속해야 함
-        System.out.println("Hello World!"); //print()
+        System.out.println("HelloWorld!"); //print("HelloWorld!")
     }
 }
