1ae7db4877896a98e2f384bbd8ce751d8dfd5783
cse1940 <se1940@naver.com>
2022-08-19 13:41:01 +0900

    with_java

---
+++ ch03_Statements/src/com/yudong80/java/ch03/SwitchBasic.java
@@ -0,0 +1,22 @@
+package com.yudong80.java.ch03;
+
+public class SwitchBasic {
+
+	public static void main(String[] args) {
+		String[] planets = {
+				"수성", "금성", "지구", "화성", "목성", "토성",
+		};
+		
+		for (String planet : planets) {
+			switch (planet) { //switch (변수)
+			case "지구":
+				System.out.println(planet + "로 우주 여행 가자!");
+				break;
+			default:
+				System.out.println(planet + "으로 우주 여행 가자!");
+			}
+		}
+
+	}
+
+}
---
+++ ch03_Statements/src/com/yudong80/java/ch03/practice_03_04.java
@@ -0,0 +1,14 @@
+//다음은 3번 예제를 switch로 변경한 코드입니다.
+package com.yudong80.java.ch03;
+
+public class practice_03_04 {
+
+	public static void main(String[] args) {
+		final int CODE_ADULT = 100;
+		final int CODE_YOUTH = 200;
+		final int CODE_CHILDREN = 300;
+		int myCode = CODE_YOUTH;
+
+	}
+
+}
