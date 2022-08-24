57c627cd6067b9e6f03924ed9fc078a7b468b658
cse1940 <se1940@naver.com>
2022-08-24 23:17:03 +0900

    with_java

---
+++ ch04_FunctionString/src/com/yudong80/java/ch04/FibonacciV1.java
@@ -0,0 +1,36 @@
+package com.yudong80.java.ch04;
+
+public class FibonacciV1 {
+
+	public static void main(String[] args) {
+				System.out.println("피보나치 수열 만들기");
+				
+				Scanner s = new Scanner(System.in);
+				System.out.println("수열 개수를 입력하세요");
+				int num = s.nextInt();
+				s.close(); //사용하면 최대한 빨리 닫아 줍니다.
+				
+				int a = 1;
+				int b = 1;
+				
+				int[] fibonacci = new int[num];
+				fibonacci[0] = a;
+				fibonacci[1] = b;
+				for(int i=1; i < num-2; ++i) {
+					fibonacci[i+2] = a + b;
+					a = b;
+					b = fibonacci[i+2];
+				}
+				System.out.println("결과: ");
+				for(int i=0; i < num; ++i) {
+					System.out.println(fibonacci[i]);
+				}
+
+			}
+
+		
+
+
+	}
+
+
