9d7548e7f2ad5ddfce14fa7bfb41ce033c54830d
cse1940 <se1940@naver.com>
2022-08-28 07:44:49 +0900

    생활코딩

---
+++ Coding/src/org/opentutorials/javatutorials/conditionaloperator/NotDemo.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.conditionaloperator;
+
+public class NotDemo {
+
+	public static void main(String[] args) {
+		// NOT !
+		if(!true) { // false
+			System.out.println(1);
+		} if(!false) { // true
+			System.out.println(2);
+		}
+
+	}
+
+}
