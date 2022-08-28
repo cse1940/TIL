3a715503795eea425dbbc6db57d00d53c0a71ec0
cse1940 <se1940@naver.com>
2022-08-28 10:25:18 +0900

    생활코딩

---
+++ Coding/src/org/openturtorials/javatutorials/loop/WhileDemo.java
@@ -0,0 +1,12 @@
+package org.openturtorials.javatutorials.loop;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		while(true) { // 반복문 while : 조건을 만족할 때까지 무한루프
+			System.out.println("Coding Everybody!");
+		}
+
+	}
+
+}
