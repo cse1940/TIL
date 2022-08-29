66c827a54c6b0ee51f9f32b2fda1b1ac7a864f58
cse1940 <se1940@naver.com>
2022-08-30 02:09:35 +0900

    생활코딩

---
+++ Tuotrials/src/org/opentutorials/javatutorials/array/ForeachDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.array;
+
+public class ForeachDemo {
+
+	public static void main(String[] args) {
+		String[] members = {"최승은", "최승혁", "빅진형"};
+		for(String e : members) {
+			System.out.println(e + "이 상담을 받았습니다.");
+		}
+
+	}
+
+}
