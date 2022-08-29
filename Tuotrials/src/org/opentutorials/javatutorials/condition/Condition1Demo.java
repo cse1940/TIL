ae94125e31b329bf2365a9f849ddafc8077185bb
cse1940 <se1940@naver.com>
2022-08-29 11:08:27 +0900

    생활코딩

---
+++ Tuotrials/src/org/opentutorials/javatutorials/condition/Condition1Demo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.condition;
+
+public class Condition1Demo {
+	 public static void main(String[] args) {
+		 // Á¶°Ç¹® : if
+	        if(true){
+	            System.out.println("result : true");
+	        }
+	 }
+
+}
