bd92ecef76334796eb060821cc81d65926716f4c
cse1940 <se1940@naver.com>
2022-09-17 17:17:03 +0900

    220917

---
+++ Web_Development/src/days_220917/DataType.java
@@ -0,0 +1,19 @@
+package days_220917;
+
+public class DataType {
+
+	public static void main(String[] args) {
+//		int (정수)
+//		정수를 저장할 수 있는 공간이 필요하면 -> 만들면 됨
+//		변수를 만드는 방법? -> 자료형 변수명;
+		
+		int age = 10; // 변수 선언과 동시에 초기화
+		int age2 = age; // 변수 선언과 동시에 초기화
+		
+		int age3; // 변수 선언
+		age3 = 0; // 선언 후 초기화
+		int age4 = age3;
+
+	}
+
+}
