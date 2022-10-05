84b488e94c027a797797321344adac2cbb4a3941
Eun <108647663+cse1940@users.noreply.github.com>
2022-10-05 18:36:27 +0900

    Delete 혼자 파이썬(1) directory

--- 혼자 파이썬(1)/list04.py
+++
@@ -1,5 +0,0 @@
-# 리스트 슬라이싱
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-print(numbers[0:5:2])
-
-print(numbers[::-1])
--- 혼자 파이썬(1)/return_only.py
+++
@@ -1,9 +0,0 @@
-# 자료 없이 리턴하기
-# 함수를 정의합니다.
-def return_test():
-    print("A 위치입니다.")
-    return # 리턴합니다.
-    print("B 위치입니다.")
-
-# 함수를 호출합니다.
-return_test()
