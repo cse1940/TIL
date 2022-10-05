6359fd25704a9344ff5dd48f86f19bfa6e4fa5d4
cse1940 <se1940@naver.com>
2022-10-05 18:42:23 +0900

    221005_upload

---
+++ src/myjava/ch02/DataType01.java
@@ -0,0 +1,35 @@
+package myjava.ch02;
+
+public class DataType01 {
+
+	public static void main(String[] args) {
+		
+		int age = 10; // 변수 선언과 동시에 초기화
+		int age2 = age; // 변수 선언과 동시에 초기화
+		
+		int age3; // 변수 선언
+		age3 = 0; // 선언 후 초기화
+		
+		int age4 = age3;
+		
+		long lage = 0;
+		lage = age4; // auto casting -> 자동적 형변환(작<큰)
+		age4 = (int)lage; // manual casting -> 형변환 필요함(큰<작)
+		
+		int in1 = 100;
+		long lo1 = 1000;
+		short sh1 = 10;
+		byte by1 = 1;
+		char ch1 = 65; // 문자를 표현하는 ascii 코드
+		
+		System.out.println(in1);
+		System.out.println(lo1);
+		System.out.println(sh1);
+		System.out.println(by1);
+		System.out.println(ch1);
+		
+//		byte < short < int < long
+
+	}
+
+}
---
+++ src/myjava/ch02/DataType05.java
@@ -0,0 +1,56 @@
+package myjava.ch02;
+
+public class DataType05 {
+
+	public static void main(String[] args) {
+
+//		문자열의 내장함수
+//		1. indexOf : 문자열 내에서 특정한 문자열의 index 값을 리턴
+//		2. substring : 문자열에서 특정한 구간의 문자열을 추출
+//		3. subSequence : substring과는 달리 char 형태로 변환함
+//		4. contains : 해당되는 문자열을 포함하고 있는지 true/false 반환
+//		5. charAt : 문자열에서 인자로 주어진 값에 해당하는 문자를 리턴
+//		6. replace : 해당 문자열로 치환
+//		7. replaceAll : replace 기능을 다 사용함과 동시에 추가로 정규식 표현 가능
+//		8. toUpperCase : 전부 대문자로 변경
+//		9. toLowerCase : 전부 소문자로 변경
+//		10. split : 문자열을 인자로 주어진 문자열일 기준으로 쪼개서 배열에 담음
+		
+		String str = "hello";
+		int index = str.indexOf('l');
+		System.out.println(index);
+		System.out.println(str.substring(0));
+		System.out.println(str.substring(index));
+		
+		str = "text.xlsx";
+		index = str.indexOf('.');
+		System.out.println("확장자는 " + str.substring(index +1));
+		System.out.println(str.subSequence(index, index+1));
+		
+		str = "Java is very funny language!";
+		boolean isContain = str.contains("very1");
+		System.out.println(isContain);
+		
+		System.out.println(str.charAt(6));
+		
+		String returnStr = str.replace("funny", "good");
+		System.out.println(str);
+		System.out.println(returnStr);
+		
+		returnStr = str.replaceAll("funny", "good");
+		System.out.println(str);
+		System.out.println(returnStr);
+		
+		returnStr = str.toUpperCase();
+		System.out.println(returnStr);
+		
+		returnStr = str.toLowerCase();
+		System.out.println(returnStr);
+		
+		String[] res = str.split(" "); // 띄어쓰기 기준으로 쪼갬
+		for(String s : res) {
+			System.out.println(s);
+		}
+	}
+
+}
