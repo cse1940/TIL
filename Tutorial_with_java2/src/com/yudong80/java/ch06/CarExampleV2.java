28cc2dfe0d8927057fa2fd779521916a81029be1
cse1940 <se1940@naver.com>
2022-10-05 15:32:22 +0900

    221005_upload

---
+++ Tutorial_with_java2/src/com/yudong80/java/ch06/CarExampleV2.java
@@ -0,0 +1,33 @@
+package com.yudong80.java.ch06;
+
+public class CarExampleV2 {
+
+	public static void main(String[] args) {
+		
+		// Car 按眉 积己 (厚眠玫 规过)
+		Car car = new Car() {
+			public void start() {
+				
+			}
+			public void accelerate() {
+				
+			}
+			public void accelerate(int km) {
+				
+			}
+			public void decelerateHalf() {
+				
+			}
+			public void decelerateAs(int km) {
+			}
+		};
+		
+		car.start();
+		car.accelerate();
+		car.accelerate(70);
+		car.decelerateAs(60);
+		car.decelerateHalf();
+
+	}
+
+}
