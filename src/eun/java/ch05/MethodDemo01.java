8015b4268d3d7fd3e8172d5f2d4d3566fc742037
cse1940 <se1940@naver.com>
2022-10-07 19:33:51 +0900

    221007_upload

--- src/eun/java/ch05/MethodDemo01.java
+++ src/eun/java/ch05/MethodDemo01.java
@@ -1,62 +1,26 @@
 package eun.java.ch05;
 
-import java.util.Scanner;
-
 public class MethodDemo01 {
 
 	public static void main(String[] args) {
 		
-//		메소드의 장점
-//		1. 코드의 재활용 -> 반복 사용
-//		2. 가독성 증가 -> 로직들의 집합
-		
-//		1. 사용자로부터 양의 정수 2개를 입력받아서 곱하는 프로그램
-		int num1 = inputNumber(), num2 = inputNumber();
-		if(num1 == -1 || num2 == -1) 
-			return;
-		fourCalc(num1, num2);
-		
-	}
-//		2. 코드의 가독성을 높인다
-		public static int inputNumber() {
-			System.out.print("0 ~ 100 값을 입력해 주세요: ");
-			Scanner sc = new Scanner(System.in);
-			int num = sc.nextInt();
-			if(num < 0 || num > 100) {
-				System.out.println("잘못 입력했어요. 입력한 값을 확인해 주세요.");
-				
-			}
-			return num;
+		int sum = 0;
+		for(int i=0; i<=10; i++) {
+			sum += i;
 		}
-		
-		public static void fourCalc(int num1, int num2) {
-			calcOperation(num1, num2, "+");
-			calcOperation(num1, num2, "-");
-			calcOperation(num1, num2, "*");
-			calcOperation(num1, num2, "/");
-			}
-		
-//		1. 코드의 재활용 -> 반복 사용
-		public static void calcOperation(int i1, int i2, String op) {
-			int num3 = 0;
-			switch(op) {
-			case "+":
-				num3 = i1 + i2;
-				System.out.println("두 변수의 합은 " + num3 + "입니다.");
-				break;
-			case "-":
-				num3 = i1 - i2;
-				System.out.println("두 변수의 차 " + num3 + "입니다.");
-				break;
-			case "/":
-				double temp = (double) i1 / i2;
-				System.out.printf("두 변수의 나눗셈은 %.2f", temp);
-				break;
-			default:
-				System.out.println("사칙연산만 가능합니다. ");
-				break;
-		}
+		System.out.println("1 ~ 10 합은 " + sum + "입니다.");
 
+		sum = 0;
+		for(int i=10; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println("10 ~ 100 합은 " + sum + "입니다.");
+		
+		sum = 0;
+		for(int i=100; i<=1000; i++) {
+			sum += i;
+		}
+		System.out.println("100 ~ 1000 합은 " + sum + "입니다.");
 	}
 
 }
--- src/eun/java/ch05/MethodDemo02.java
+++ src/eun/java/ch05/MethodDemo02.java
@@ -1,23 +1,28 @@
 package eun.java.ch05;
 
-import java.util.Arrays;
-
 public class MethodDemo02 {
 
 	public static void main(String[] args) {
 		
-		int a = 100;
-		int b = a;
-		b = 200;
-		System.out.println(a); // a는 값이 안 바뀜
-		System.out.println(b);
-		
-		int[] ary1 = {1, 2, 3, 4, 5};
-		int[] ary2 = ary1;
-		
-		ary2[0] = 1000;
-		System.out.println(Arrays.toString(ary1));
-		System.out.println(Arrays.toString(ary2));
+		System.out.println("1 ~ 10 합은 " + sum(1, 10) + "입니다.");
+		System.out.println("10 ~ 100 합은 " + sum(10, 100) + "입니다.");
+		System.out.println("100 ~ 1000 합은 " + sum(100, 1000) + "입니다.");
+	}
+	
+//	메서드를 이용하면 얻을 수 있는 장점
+//	1. 중복 코드를 줄이고 코드를 재사용할 수 있다
+//	2. 코드를 모듈화해 가독성을 높이므로 프로그램의 품질을 향상시킨다
+	public static int sum(int i1, int i2) { // method
+//	public : 접근 지정자
+//	static : 객체를 생성하지 않고 실행할 수 있다(static은 static끼리 논다)
+//	int : 반환 타입
+//	sum : 메서드 이름
+//	int i1, int i2 : 매개변수 목록
+		int sum = 0; // 지역 변수
+		for(int i=i1; i<=i2; i++) {
+			sum += i;
+		}
+		return sum;
 	}
 
 }
