c95ecb0b3f6ca5b2af283e55053dfdfe1fab2e74
cse1940 <se1940@naver.com>
2022-10-09 13:04:02 +0900

    221009_upload

---
+++ src/org/opentutorials/javatutorials/method/MethodDemo2.java
@@ -0,0 +1,31 @@
+package org.opentutorials.javatutorials.method;
+
+public class MethodDemo2 {
+
+	public static void main(String[] args) {
+		
+//		만약 메소드가 없다면?
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+//		이것을 1000번 해야 한다면? 각각의 로직이 1000 줄에 육박한다면?
+//		메소드를 사용한다면 이러한 문제를 현저히 줄일 수 있다
+
+	}
+
+}
