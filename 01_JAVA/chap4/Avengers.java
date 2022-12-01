fd07dce7a371e46fcbaac15ffa61f93daa875580
cse1940 <se1940@naver.com>
2022-12-02 00:05:23 +0900

    221202_upload

---
+++ 01_JAVA/chap4/Avengers.java
@@ -0,0 +1,10 @@
+package chap4;
+
+public class Avengers {
+    public static void main(String[] args) {
+        double score = 9.82;
+
+        System.out.println("어벤져스4 평점:");
+        System.out.println(score);
+    }
+}
