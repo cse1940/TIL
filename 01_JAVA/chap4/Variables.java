36709a52086eec120bdf7ea792689068d7ac7762
cse1940 <se1940@naver.com>
2022-12-02 00:05:36 +0900

    221202_upload

---
+++ 01_JAVA/chap4/Variables.java
@@ -0,0 +1,15 @@
+package chap4;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "이나라";
+        int age = 25;
+        double weight = 54.2;
+        boolean isMale = false;
+
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("몸무게: " + weight + "kg");
+        System.out.println("남성인가요? " + isMale);
+    }
+}
