8c638587916a4715ff61de54796f7283eef2605f
cse1940 <se1940@naver.com>
2022-12-02 00:05:28 +0900

    221202_upload

---
+++ 01_JAVA/chap4/Greetings.java
@@ -0,0 +1,14 @@
+package chap4;
+
+public class Greetings {
+    public static void main(String[] args) {
+        String name = "최승은";
+
+        System.out.println("안녕하세요~ 반갑습니다! 제 이름은,");
+        System.out.println(name);
+        System.out.println("입니다. 잘 부탁드립니다~");
+    }
+   
+
+    
+}
