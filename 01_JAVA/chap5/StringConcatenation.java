af1823f160b1fbffeba61a88724af21026c12d56
cse1940 <se1940@naver.com>
2022-12-02 00:06:20 +0900

    221202_upload

---
+++ 01_JAVA/chap5/StringConcatenation.java
@@ -0,0 +1,10 @@
+package chap5;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String text = "hello" + " world!";
+        text = text + " number " + 5;
+
+        System.out.println(text);
+    }
+}
