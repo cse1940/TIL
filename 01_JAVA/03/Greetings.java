01e2b32fd782cf7a86dae0238ce83660ae6edc83
cse1940 <se1940@naver.com>
2022-12-01 14:05:46 +0900

    221201_upload

---
+++ 01_JAVA/03/Greetings.java
@@ -0,0 +1,7 @@
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요! 반갑습니다.");
+        System.out.println("열혈 청년 홍길동 입니다.");
+        System.out.println("잘 부탁 드립니다.");
+    }
+}
