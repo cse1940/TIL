733da53ead6fff148934da74ef22b85419cc99a6
cse1940 <se1940@naver.com>
2022-12-02 00:05:34 +0900

    221202_upload

---
+++ 01_JAVA/chap4/TangSoo6.java
@@ -0,0 +1,10 @@
+package chap4;
+
+public class TangSoo6 {
+    public static void main(String[] args) {
+        boolean isBoomuk = true;
+
+        System.out.println("당신은 부먹파입니까?");
+        System.out.println(isBoomuk);
+    }
+}
