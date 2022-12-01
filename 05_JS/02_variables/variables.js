5af841e4a27d3a274ad2126df0fcdbf2d28936d5
cse1940 <se1940@naver.com>
2022-12-01 13:51:49 +0900

    221201_upload

---
+++ 05_JS/02_variables/variables.js
@@ -0,0 +1,23 @@
+// 주석 : 실행 되지 않는 코드, 이해를 돕기 위해 작성!
+// 엄격모드 적용 : 초창기 자바스크립트의 단점을 보완하는 설정
+// (좀 더 엄격한 검증 적용!)
+'use strict';
+
+// 1. 변수, 열린 상자 만들기!
+let openBox = "망고";
+console.log(openBox);
+
+openBox = "바둑이";
+console.log(openBox);
+
+// 2. 이름이 같은 변수, 생성 불가!
+// let openBox = "도너츠";
+openBox = "도너츠";
+console.log(openBox);
+
+// 3. 상수, 잠긴 상자 만들기!
+const cookieBox = "쿠키";
+console.log(cookieBox);
+
+// cookieBox = "과일";
+
