ca3cc7bfb4c54624f5ea019424a1cfeabc393e43
cse1940 <se1940@naver.com>
2022-12-03 00:02:51 +0900

    221203_upload

---
+++ 01_JAVA/chap10/Multiply.java
@@ -0,0 +1,12 @@
+package chap10;
+
+public class Multiply {
+    public static void main(String[] args) {
+        times(2, 2);
+        times(3, 4);
+    }
+
+    public static void times(double a, double b) {
+        System.out.println(a * b);
+    }
+}
