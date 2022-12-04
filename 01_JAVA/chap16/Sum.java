c04290b6daa482ff49de1e32001680aa93e851aa
cse1940 <se1940@naver.com>
2022-12-04 22:14:33 +0900

    221204_upload

---
+++ 01_JAVA/chap16/Sum.java
@@ -0,0 +1,11 @@
+package chap16;
+
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=0; i<11; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
