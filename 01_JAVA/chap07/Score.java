1526614e17659b099ea1890f632c8d0b1e20554b
cse1940 <se1940@naver.com>
2022-12-07 00:01:18 +0900

    221207_upload

---
+++ 01_JAVA/chap07/Score.java
@@ -0,0 +1,11 @@
+package chap07;
+
+public class Score {
+    public static void main(String[] args) {
+        int math = 96, science = 88, english = 76;
+
+        System.out.println("수학: " + math);
+        System.out.println("과학: " + science);
+        System.out.println("영어: " + english);
+    }
+}
