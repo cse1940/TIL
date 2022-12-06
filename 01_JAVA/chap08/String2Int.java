ac1a5d3a701a89e10c200a18288b75b7d5a037ef
cse1940 <se1940@naver.com>
2022-12-07 00:01:28 +0900

    221207_upload

---
+++ 01_JAVA/chap08/String2Int.java
@@ -0,0 +1,11 @@
+package chap08;
+
+public class String2Int {
+    public static void main(String[] args) {
+        int seven = Integer.parseInt("7");
+        double pi = Double.parseDouble("3.14");
+
+        System.out.println("seven = " + seven);
+        System.out.println("pi = " + pi);
+    }
+}
