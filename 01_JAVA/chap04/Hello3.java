a6f8ec127203e612446d928c3119ffe9eae8b806
cse1940 <se1940@naver.com>
2022-12-06 16:51:11 +0900

    221206_upload

---
+++ 01_JAVA/chap04/Hello3.java
@@ -0,0 +1,10 @@
+package chap04;
+
+public class Hello3 {
+    public static void main(String[] args) {
+        String foo = "자바";
+        System.out.println(foo);
+        foo = "프로그래밍";
+        System.out.println(foo);
+    }
+}
