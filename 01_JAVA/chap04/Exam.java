0101f4147b522fbd7f2f9c52f959b0857478ace0
cse1940 <se1940@naver.com>
2022-12-06 16:51:05 +0900

    221206_upload

---
+++ 01_JAVA/chap04/Exam.java
@@ -0,0 +1,13 @@
+package chap04;
+
+public class Exam {
+    public static void main(String[] args) {
+        int middleScore = 68;
+        int finalScore = 88;
+
+        System.out.println("승은이의 중간점수:");
+        System.out.println(middleScore);
+        System.out.println("승은이의 기말점수:");
+        System.out.println(finalScore);
+    }
+}
