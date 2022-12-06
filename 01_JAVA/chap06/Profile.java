d8834d6f33275fab122b7f48e6f0669279209b56
cse1940 <se1940@naver.com>
2022-12-06 16:51:28 +0900

    221206_upload

---
+++ 01_JAVA/chap06/Profile.java
@@ -0,0 +1,15 @@
+package chap06;
+
+public class Profile {
+    public static void main(String[] args) {
+        String name = "최승은";
+        int studentNumber = 2019144044;
+        double height = 1.6;
+        boolean isMale = false;
+
+        System.out.println("이름: " + name);
+        System.out.println("학번: " + studentNumber);
+        System.out.println("신장: " + height + "m");
+        System.out.println("남자인가요? " + isMale);
+    }
+}
