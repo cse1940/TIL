8fba50ebeb74ab4e6f112c3928dcdf87c1cf475b
cse1940 <se1940@naver.com>
2022-12-07 00:01:26 +0900

    221207_upload

---
+++ 01_JAVA/chap08/DivTest.java
@@ -0,0 +1,17 @@
+package chap08;
+
+public class DivTest {
+    public static void main(String[] args) {
+        double a = 5.0 / 2.0; // 2.5
+        System.out.println("a = " + a);
+
+        int b = 4 / 2; // 2
+        System.out.println("b = " + b);
+
+        int c = 5 / 2; // 2
+        System.out.println("c = " + c);
+
+        double d = 5 / 2; // 2.0
+        System.out.println("d = " + d);
+    }
+}
