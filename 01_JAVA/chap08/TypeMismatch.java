61c3ba112ff0df835ff78915d96baa5ff140c1d9
cse1940 <se1940@naver.com>
2022-12-08 12:42:17 +0900

    221208_upload

---
+++ 01_JAVA/chap08/TypeMismatch.java
@@ -0,0 +1,10 @@
+package chap08;
+
+public class TypeMismatch {
+    public static void main(String[] args) {
+//      int five = 5;
+        String five = "5";
+
+        System.out.println(five);
+    }
+}
