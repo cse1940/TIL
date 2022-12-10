0657887c4c5338981457c8a86edb762619d688a2
cse1940 <se1940@naver.com>
2022-12-10 23:19:13 +0900

    221210_modify

--- 05_JS/01_helloworld/hello.js
+++ 05_JS/01_helloworld/hello.js
@@ -4,7 +4,7 @@ const btn = document.querySelector("button");
 // 2. 버튼 클릭 시, 특정 동작 수행
 btn.addEventListener("click", helloMbti);
 
-// 3. 특정 동작 내용
+// 3. 특정 동작 내용 
 function helloMbti() {
     const heading = document.querySelector("h1");
     const mbti = prompt("당신의 MBTI는 무엇입니까?");
